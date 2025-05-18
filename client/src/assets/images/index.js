f1946dced00bbdd9b4c60dfab7d2bf631cf6fbf1
thongtoan <n.thongtoan@gmail.com>
2025-05-18 20:40:05 +0700

    Set up code base client

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,6 @@
+import reactLogo from "./react.svg";
+import viteLogo from "./vite.svg";
+import backgroundMobile from "./bg-intro-mobile.png";
+import backgroundDesktop from "./bg-intro-desktop.png";
+
+export { reactLogo, viteLogo, backgroundMobile, backgroundDesktop };
